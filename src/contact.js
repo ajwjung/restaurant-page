36368eb12dfe2e6f05d589f0542f7ed500f8d92b
Amy Jung <ajwjung@outlook.com>
2022-12-19 15:27:28 -0800

    Create contact page

---
+++ src/contact.js
@@ -0,0 +1,44 @@
+import Page from "./page-load";
+import { MainElements, OtherElements } from "./elements-builder";
+
+const ContactElements = (() => {
+    const createContactHeader = () => {
+        const header = MainElements.createHeader();
+        
+        const contactH1 = OtherElements.createH1("Contact Us");
+        header.appendChild(contactH1);
+
+        return header;
+    }
+
+    const createContactMain = () => {
+        const main = MainElements.createMain();
+        const mainContainer = OtherElements.createDiv("contact-info");
+    
+        const contactPara = OtherElements.createPara("We look forward to serving you. If you have any questions or would like to book a reservation, please reach out to us.")
+        const contactPhone = OtherElements.createPara("111-111-1111");
+        const contactEmail = OtherElements.createPara("info@gusteaus.com");
+
+        mainContainer.appendChild(contactPara);
+        mainContainer.appendChild(contactPhone);
+        mainContainer.appendChild(contactEmail);
+        main.append(mainContainer);
+    
+        return main;
+    }
+
+    return { createContactHeader, createContactMain };
+
+})();
+
+const Contact = (() => {
+    const content = Page.content;
+
+    const contactHeader = ContactElements.createContactHeader();
+    const contactMain = ContactElements.createContactMain();
+    
+    content.appendChild(contactHeader);
+    content.appendChild(contactMain);
+})();
+
+export default Contact;

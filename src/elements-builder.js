7f9ca3912bbe45990c41807aac80afbad67b3065
Amy Jung <ajwjung@outlook.com>
2022-12-19 13:10:36 -0800

    refactor: Separate element creator module patterns

---
+++ src/elements-builder.js
@@ -0,0 +1,62 @@
+const MainElements = (() => {
+    const createHeader = () => {
+        const newHeader = document.createElement("header");
+        return newHeader;
+    }
+
+    const createMain = () => {
+        const newMain = document.createElement("main");
+        return newMain;
+    }
+
+    const createSection = (className) => {
+        const newSection = document.createElement("section");
+        newSection.classList.add(className);
+        return newSection;
+    }
+
+    return { createHeader, createMain, createSection }
+})();
+
+const OtherElements = (() => {
+    const createH1 = (text) => {
+        const newH1 = document.createElement("h1");
+        newH1.textContent = text;
+        return newH1;
+    }
+
+    const createH2 = (text) => {
+        const newH2 = document.createElement("h2");
+        newH2.textContent = text;
+        return newH2;
+    }
+
+    const createDiv = (className) => {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add(className);
+        return newDiv;
+    }
+
+    const createImage = (src) => {
+        const newImage = document.createElement("img");
+        newImage.src = src;
+        return newImage;
+    }
+
+    const createPara = (text) => {
+        const newPara = document.createElement("p");
+        newPara.textContent = text;
+        return newPara;
+    }
+
+    const createSpan = (text) => {
+        const newSpan = document.createElement("span");
+        newSpan.textContent = text;
+        return newSpan;
+    }
+
+    return { createH1, createH2, createDiv,
+        createImage, createPara, createSpan }
+})();
+
+export { MainElements, OtherElements };
--- src/page-load.js
+++ src/page-load.js
@@ -1,31 +1,32 @@
-const MainElements = (() => {
-    const createHeader = () => {
-        const newHeader = document.createElement("header");
+import { MainElements, OtherElements } from "./elements-builder";
+
+const PageElements = (() => {
+    const CreateHeaderSection = (() => {
+        const header = MainElements.createHeader();
 
         const restaurantLogo = OtherElements.createImage("../src/imgs/gusteaus.png");
         const restaurantName = OtherElements.createH1("Gusteau's");
         
-        newHeader.appendChild(restaurantLogo);
-        newHeader.appendChild(restaurantName);
+        header.appendChild(restaurantLogo);
+        header.appendChild(restaurantName);
 
-        return newHeader;
-    }
+        return header;
+    })
 
-    const createMain = () => {
-        const newMain = document.createElement("main");
+    const newMain = MainElements.createMain();
 
-        // Chefs Section
-        const chefsSection = OtherElements.createSection("chefs");
+    const CreateChefSection = () => {
+        const chefsSection = MainElements.createSection("chefs");
         const chefsH2 = OtherElements.createH2("Meet the Chefs");
-
+    
         const alfredoDiv = OtherElements.createDiv("alfredo");
         const alfredoImg = OtherElements.createImage("../src/imgs/alfredo-linguini.png");
         const alfredoNamePara = OtherElements.createPara("Alfredo Linguini");
-
+    
         const remyDiv = OtherElements.createDiv("remy");
         const remyImg = OtherElements.createImage("../src/imgs/remy.png");
         const remyNamePara = OtherElements.createPara("Remy");
-
+    
         alfredoDiv.appendChild(alfredoImg);
         alfredoDiv.appendChild(alfredoNamePara);
         remyDiv.appendChild(remyImg);
@@ -35,8 +36,12 @@ const MainElements = (() => {
         chefsSection.appendChild(remyDiv);
         newMain.appendChild(chefsSection);
 
-        // Reviews Section
-        const reviewsSection = OtherElements.createSection("reviews");
+        return chefsSection;
+    }
+
+    const CreateReviewSection = () => {
+        const reviewsSection = MainElements.createSection("reviews");
+
         const reviewsH2 = OtherElements.createH2("What the Critics Say");
         const egoImg = OtherElements.createImage("../src/imgs/anton-ego.png");
         const egoReview = "I experienced something new: an extraordinary meal from a singularly unexpected source. [...] It is difficult to imagine more humble origins than those of the genius now cooking at Gusteau's who is, in this critic's opinion, nothing less than the finest chef in France. I will be returning to Gusteau's soon, hungry for more.";
@@ -49,68 +54,25 @@ const MainElements = (() => {
         reviewsSection.appendChild(egoNamePara);
         newMain.appendChild(reviewsSection);
 
-        return newMain;
+        return reviewsSection;
     }
 
-    return { createHeader, createMain }
-})();
-
-
-const OtherElements = (() => {
-    const createH1 = (text) => {
-        const newH1 = document.createElement("h1");
-        newH1.textContent = text;
-        return newH1;
-    }
-
-    const createH2 = (text) => {
-        const newH2 = document.createElement("h2");
-        newH2.textContent = text;
-        return newH2;
-    }
-
-    const createSection = (className) => {
-        const newSection = document.createElement("section");
-        newSection.classList.add(className);
-        return newSection;
-    }
-
-    const createDiv = (className) => {
-        const newDiv = document.createElement("div");
-        newDiv.classList.add(className);
-        return newDiv;
-    }
-
-    const createImage = (src) => {
-        const newImage = document.createElement("img");
-        newImage.src = src;
-        return newImage;
-    }
-
-    const createPara = (text) => {
-        const newPara = document.createElement("p");
-        newPara.textContent = text;
-        return newPara;
-    }
-
-    const createSpan = (text) => {
-        const newSpan = document.createElement("span");
-        newSpan.textContent = text;
-        return newSpan;
-    }
-
-    return { createH1, createH2, createSection,
-        createDiv, createImage, createPara, createSpan }
+    return { CreateHeaderSection, CreateChefSection, CreateReviewSection };
 })();
 
 const Page = (() => {
     const content = document.querySelector("#content");
 
-    const header = MainElements.createHeader();
-    const main = MainElements.createMain();
+    const header = PageElements.CreateHeaderSection();
+    const chefsMain = PageElements.CreateChefSection();
+    const reviewsMain = PageElements.CreateReviewSection();
 
     content.appendChild(header);
-    content.appendChild(main);
+    content.appendChild(chefsMain);
+    content.appendChild(reviewsMain);
+
+    return { content };
 })();
 
 export default Page;
+export { MainElements, OtherElements };
